e43a82180de4e8cea7ffdee056278f1adb9b4a74
alessandrolega <alessandrolega24@gmail.com>
2022-11-28 02:32:21 +0100

    creata cli + import bootstrap, axios + get API

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+
+  data(){
+    return {
+
+    }
+  },
+
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((response)=>{
+      console.log(response)
+
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
